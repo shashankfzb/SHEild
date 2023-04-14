dbcb8923d5a821ab458f2dc8c445cd517aa12d20
Xelta69 <93995828+Xelta69@users.noreply.github.com>
2023-04-14 13:02:03 +0530

    Update main.dart

--- flutter_lib/main.dart
+++ flutter_lib/main.dart
@@ -1,6 +1,9 @@
 // import 'dart:html';
 
+import 'dart:io';
+
 import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:she_ild/splash_screen.dart';
 
@@ -15,6 +18,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      initialRoute: "/",
+      routes: {
+        "/first": (context) =>  MyHomePage(title: "DEMO yaar"),
+        "/second": (context) => SecondScreen()
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -32,12 +40,25 @@ class MyApp extends StatelessWidget {
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
       // home: Splash(),
       home: AnimatedSplashScreen(
+
         duration: 3000,
-        splash: Icons.home,
         nextScreen: MyHomePage(title: 'DEMO'),
+        splash: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                // image: NetworkImage('C:/Users/Udyan/StudioProjects/she_ild/lib/Clip_path_group.png'),
+                // image: AssetImage('Clip_path_group.png'),
+                image: FileImage(File("C:/Users/Udyan/StudioProjects/she_ild/lib/Clip_path_group.png")),
+                fit: BoxFit.cover
+            ) ,
+          ),
+        ),
+        // backgroundColor: Colors.blue,
         splashTransition: SplashTransition.rotationTransition,
         // pageTransitionType: PageTransitionType.scale,
-        backgroundColor: Colors.blue
+        //   splash: Icons.home,
+
+
       )
     );
   }
@@ -62,68 +83,66 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     // This call to setState tells the Flutter framework that something has
+  //     // changed in this State, which causes it to rerun the build method below
+  //     // so that the display can reflect the updated values. If we changed
+  //     // _counter without calling setState(), then the build method would not be
+  //     // called again, and so nothing would appear to happen.
+  //     _counter++;
+  //   });
+
+  
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        
+        child: OutlinedButton(
+
+          onPressed: (){
+            Navigator.push(
+              context, CupertinoPageRoute(builder: (context)=> SecondScreen()), 
+              );
+          },
+          child: Image.asset("C:/Users/Udyan/StudioProjects/she_ild/lib/Group_1.png")
+           
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+          // ],
+        ),
+      );
+    
+  }
+}
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: OutlinedButton(
+
+          onPressed: (){ },
+          // child: Image.asset("Group_1.png")
+          child: Text("ACCHA AISA ")
+          
+        ),
+          // ],
+        ),
+      );
   }
 }
