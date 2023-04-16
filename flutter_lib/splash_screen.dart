592f480af04c9efa21b0359ec627b26e36c1f701
Xelta69 <93995828+Xelta69@users.noreply.github.com>
2023-04-16 17:32:07 +0530

    Update splash_screen.dart

--- flutter_lib/splash_screen.dart
+++ flutter_lib/splash_screen.dart
@@ -18,11 +18,17 @@ class _SplashState extends State<Splash> {
   @override
   void initState(){
     super.initState();
-    // _navigatetohome();
+    _navigatetohome();
   }
+  double opacityLevel = 1.0;
+
+    void _changeOpacity() {
+    setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
+    }
 
   _navigatetohome()async{
     await Future.delayed(Duration(milliseconds: 2000), (){});
+    _changeOpacity();    
     var pushReplacement = Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>MyHomePage(title: 'Demo')));
   }
 
@@ -37,13 +43,58 @@ class _SplashState extends State<Splash> {
         home: Scaffold(
             body: Center(
                 child: Container(
-                    child: Text('SHE-ild', style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold
-                    ))
+                  padding: const EdgeInsets.all(16.0),
+                  width: double.infinity,
+                  height: double.infinity,
+                  alignment: Alignment.center,
+                    // child: Text('SHE-ild', style: TextStyle(
+                    //     fontSize: 24,
+                    //     fontWeight: FontWeight.bold
+                    
+                    // )
+                    // ),
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage("lib/Clip_path_group.png"),
+                            // image: AssetImage('Clip_path_group.png'),
+                            // image: FileImage(File("C:/Users/Udyan/StudioProjects/she_ild/lib/Clip_path_group.png")),
+                            fit: BoxFit.cover
+                        ),
                 )
             )
         )
+      )
+    );
+  }
+}
+
+class LogoFade extends StatefulWidget {
+  @override
+  createState() => LogoFadeState();
+}
+
+class LogoFadeState extends State<LogoFade> {
+  double opacityLevel = 1.0;
+
+  void _changeOpacity() {
+    setState(() => opacityLevel = opacityLevel == 0 ? 1.0 : 0.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedOpacity(
+          opacity: opacityLevel,
+          duration: Duration(seconds: 3),
+          child: FlutterLogo(),
+        ),
+        ElevatedButton(
+          child: Text('Fade Logo'),
+          onPressed: _changeOpacity,
+        ),
+      ],
     );
   }
 }
